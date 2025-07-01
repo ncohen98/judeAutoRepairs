23df57e1e131d83597cd3b04ae8acc94105a1a82
Hadas Cohen <hc820@live.mdx.ac.uk>
2025-07-01 23:37:16 +0100

    updated pages

--- src/app/(tabs)/layout.tsx
+++ src/app/(tabs)/layout.tsx
@@ -1,6 +1,8 @@
-// src/app/(tabs)/layout.tsx
+// app/(tabs)/layout.tsx (server component)
 import React from "react";
-import TabsLayout from "@/components/TabsLayout";
+import dynamic from "next/dynamic";
+
+const TabsLayout = dynamic(() => import('@/components/TabsLayout'), { ssr: false });
 
 export default function TabsGroupLayout({ children }: { children: React.ReactNode }) {
   return <TabsLayout>{children}</TabsLayout>;
