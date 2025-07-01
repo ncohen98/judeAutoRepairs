155c009179e113d09f05f6c5ca48e50e2d5169e1
Hadas Cohen <hc820@live.mdx.ac.uk>
2025-07-01 23:51:37 +0100

    temp - removed tabs folder

--- src/app/(tabs)/layout.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from "react";
-import TabsLayout from "@/components/TabsLayout";
-
-export default function TabsGroupLayout({ children }: { children: React.ReactNode }) {
-  return <TabsLayout>{children}</TabsLayout>;
-}
